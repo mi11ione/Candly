3f0ba905eb3b2cf8128ef3f7884cc72d05470219
mi11ione <i@mi11ion.io>
2024-08-06 15:55:03 +0500

    [fix]: fixed thread 3 exc error

--- CoreDI/Sources/CoreDI/Implementations/AppDependency.swift
+++ CoreDI/Sources/CoreDI/Implementations/AppDependency.swift
@@ -1,28 +1,27 @@
 import CoreRepository
 import ErrorHandling
-import Foundation
 import NetworkService
 import SwiftData
+import Foundation
 
 @Observable
 public class AppDependency: Dependency {
+    private let modelContainer: ModelContainer
     private let modelContext: ModelContextWrapperProtocol
     private let errorHandler: ErrorHandling
     private let networkService: NetworkServiceProtocol
     private let patternRepository: PatternRepositoryProtocol
     private let tickerRepository: TickerRepositoryProtocol
+    private let cacheExpirationInterval: TimeInterval
 
     public init(cacheExpirationInterval: TimeInterval = 120) {
-        do {
-            let modelContainer = try ModelContainer()
-            modelContext = ModelContextWrapper(modelContainer.mainContext)
-            errorHandler = DefaultErrorHandler()
-            networkService = NetworkService(errorHandler: errorHandler)
-            patternRepository = PatternRepository(modelContext: modelContext, errorHandler: errorHandler)
-            tickerRepository = TickerRepository(modelContext: modelContext, networkService: networkService, errorHandler: errorHandler, cacheExpirationInterval: cacheExpirationInterval)
-        } catch {
-            fatalError("Failed to initialize AppDependency: \(error)")
-        }
+        modelContainer = try! ModelContainer()
+        modelContext = ModelContextWrapper(modelContainer.mainContext)
+        errorHandler = DefaultErrorHandler()
+        networkService = NetworkService(errorHandler: errorHandler)
+        patternRepository = PatternRepository(modelContext: modelContext, errorHandler: errorHandler)
+        self.cacheExpirationInterval = cacheExpirationInterval
+        tickerRepository = TickerRepository(modelContext: modelContext, networkService: networkService, errorHandler: errorHandler, cacheExpirationInterval: cacheExpirationInterval)
     }
 
     public func makePatternRepository() -> PatternRepositoryProtocol { patternRepository }
