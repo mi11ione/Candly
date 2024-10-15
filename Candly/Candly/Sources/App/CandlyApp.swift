e1d5290cdb23018c54564a8c24535bf5c69b5e7f
mi11ione <i@mi11ion.io>
2024-10-15 22:48:55 +0300

    [build]: added rocketsim

--- Candly/Candly/Sources/App/CandlyApp.swift
+++ Candly/Candly/Sources/App/CandlyApp.swift
@@ -2,9 +2,24 @@ import SwiftUI
 
 @main
 struct CandlyApp: App {
+    
+    init() {
+        loadRocketSimConnect()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
+}
+
+private func loadRocketSimConnect() {
+    #if DEBUG
+    guard (Bundle(path: "/Applications/RocketSim.app/Contents/Frameworks/RocketSimConnectLinker.nocache.framework")?.load() == true) else {
+        print("Failed to load linker framework")
+        return
+    }
+    print("RocketSim Connect successfully linked")
+    #endif
 }
